16e20421598a7637e3751cf1d96107ce61d96627
A S M Humaun Kabir <psychopath.humaun@gmail.com>
2015-11-27 12:12:30 +0600

    Create uri 1007.c

---
+++ uri 1007.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  
+int main() {
+  
+    int A, B, C, D;
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+    int h = (A * B - C * D);
+    printf("DIFERENCA = %d\n", h);
+  
+    return 0;
+}
